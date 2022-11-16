9847abfc74b6a15c24d1f8b8ec65e8440a424abc
priorfire4411 <craft1.brian@gmail.com>
2022-11-15 19:12:26 -0600

    update
    update

--- tests/transformers/test_mean_center.py
+++ tests/transformers/test_mean_center.py
@@ -1,10 +1,23 @@
-from sk_extend.transformers.mean_center import MeanCenteredScaler
+#from sk_extend.transformers.mean_center import MeanCenteredScaler
 import pandas as pd
 
-def test_mean_centered_scaler(get_dataframe):
-	scaler = MeanCenteredScaler()
-	new_df = scaler.fit_transform(get_dataframe)
-	expected = pd.DataFrame({
-		'col_a': [-50.0, 0.0, 50.0]
-		})
-	assert new_df.equals(expected)
+# def test_mean_centered_scaler(get_dataframe):
+# 	scaler = MeanCenteredScaler()
+# 	new_df = scaler.fit_transform(get_dataframe)
+# 	expected = pd.DataFrame({
+# 		'col_a': [-50.0, 0.0, 50.0]
+# 		})
+# 	#assert new_df.equals(expected)
+# 	assert 5 == 4
+
+def test_a():
+	assert 5 == 5
+
+def test_b():
+	assert 5 == 5
+
+def test_c():
+	assert 5 == 5
+
+def test_d():
+	assert 5 == 4
